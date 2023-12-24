f08c3e47a75ea25417a05fc5527b526141310246
damlakayikci <damla.kayikci@boun.edu.tr>
2023-12-24 10:21:57 +0300

    update: refactor work.py

--- src/work.py
+++ src/work.py
@@ -7,7 +7,7 @@ parent_comm = MPI.Comm.Get_parent()                                 # get the pa
 rank = parent_comm.Get_rank()                                       # get the rank of the current machine
 
 _, filename = sys.argv                                              # get the filename from the maim process
-_, threshold, _ = init.get_input(str(filename))                     # initialize the machines and get the threshold value
+threshold = init.get_input(str(filename))                     # initialize the machines and get the threshold value
 
 if rank != 0:                                                       # if not the center
     Machine.machines[rank].work(parent_comm, threshold)             # call work function for the current machine
